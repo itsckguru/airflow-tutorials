91d857ba3de135189f6f16989f42f85790c77113
itsckguru <chandan@propertyguru.tech>
2024-07-07 12:05:44 +0530

    feat: elasticseach configuration and elastic hooks

--- dags/xcom_dag.py
+++ dags/xcom_dag.py
@@ -1,26 +1,24 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
-from airflow.operators.python_operator import BranchPythonOperator
- 
+
 from datetime import datetime
- 
+
 def _t1(ti):
     ti.xcom_push(key='my_key', value=42)
- 
+
 def _t2(ti):
     ti.xcom_pull(key='my_key', task_ids='t1')
 
 def _branch(ti):
-    xcom_value = ti.xcom_pull(task_ids='t1', key='my_key')
-    if xcom_value == 42:
-        return 't3'
-    else:
-        return 't4'
- 
+    value = ti.xcom_pull(key='my_key', task_ids='t1')
+    if (value == 42):
+        return 't2'
+    return 't3'
+
 with DAG("xcom_dag", start_date=datetime(2022, 1, 1), 
     schedule_interval='@daily', catchup=False) as dag:
- 
+    
     t1 = PythonOperator(
         task_id='t1',
         python_callable=_t1
@@ -28,17 +26,22 @@ with DAG("xcom_dag", start_date=datetime(2022, 1, 1),
     branch = BranchPythonOperator(
         task_id='branch',
         python_callable=_branch
-
     )
- 
+
     t2 = PythonOperator(
         task_id='t2',
         python_callable=_t2
     )
- 
+
     t3 = BashOperator(
         task_id='t3',
         bash_command="echo ''"
     )
- 
-    t1 >> branch >> [t2, t3]
+
+    t4 = BashOperator(
+        task_id='t4',
+        bash_command="echo ''",
+        trigger_rule='none_failed_min_one_success'
+    )
+
+    t1 >> branch >> [t2, t3] >> t4
